ceef9fafaa9b2364c3733711dfef98af74850eb9
Ziyuan Zhang <61440144+WAcry@users.noreply.github.com>
2021-11-12 05:38:31 -0400

    add cmd demo2

--- src/config/config.go
+++ src/config/config.go
@@ -3,4 +3,8 @@ package config
 var Path = map[string]string{
 	"bash": "C:\\cygwin64\\bin\\bash.exe",
 	// "bash": "/bin/bash",
+	"python": "C:\\Users\\david\\anaconda3\\python.exe",
+	// "python": "/usr/bin/python",
+	"hello.py": "src\\demo\\go\\demo2\\hello.py",
+	// "hello.py": "src/demo/go/demo2/hello.py",
 }
---
+++ src/demo/go/demo2/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"GDTS/src/config"
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	var (
+		cmd    *exec.Cmd
+		output []byte
+		err    error
+	)
+
+	// Generate cmd
+	cmd = exec.Command(config.Path["bash"], "-c", "/usr/bin/sleep 5;/usr/bin/pwd;/usr/bin/ls -l")
+
+	// execute command, capture the output of the child process from the pipe
+	if output, err = cmd.CombinedOutput(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// print output of the child process
+	fmt.Println(string(output))
+
+	cmd = exec.Command(config.Path["python"], config.Path["hello.py"])
+
+	if output, err = cmd.CombinedOutput(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(string(output))
+}
