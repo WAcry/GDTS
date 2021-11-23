9682bc9ae2ce92e1834816fb1217647cb6866598
Ziyuan Zhang <61440144+WAcry@users.noreply.github.com>
2021-11-22 20:41:16 -0400

    add etcd demo1

---
+++ src/demo/go_etcd/demo1/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"config"
+	"fmt"
+	"time"
+	"utils/coreos/etcd/clientv3"
+)
+
+func main() {
+	var (
+		conf   clientv3.Config
+		client *clientv3.Client
+		err    error
+	)
+
+	// client config
+	conf = clientv3.Config{
+		Endpoints: []string{config.Server["etcd"]},
+		//Endpoints:   []string{"21.281.122.24:2379", "21.281.122.39:2379", "21.281.122.21:2379"}, // cluster endpoints
+		DialTimeout: 5 * time.Second,
+	}
+
+	// create connection
+	if client, err = clientv3.New(conf); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	client = client
+}
